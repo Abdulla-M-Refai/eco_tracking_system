ec45921b616163b6be25d9514f437e9923fd9c0f
Abdulla-M-Refai <abdullah.m.refai@gmail.com>
2023-11-23 15:07:04 +0200

    create project structure

---
+++ src/test/java/com/eco/track/eco_tracking_system/EcoTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eco.track.eco_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
